fc6c815143f2858c4b527f325910c638b115e17a
lambaaryan011 <aryanlamba54@gmail.com>
2024-03-07 23:45:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import  './App.css'
+export default function App() {
+  return (
+    <>
+    <div className='parent'>
+      <div className="item-1">item 1</div>
+      <div className="item-2">item 2</div>
+      <div className="item-3"> item 3</div>
+      
+    </div >
+    <div className="parent-grid">
+    <div className="item-a">item 1</div>
+    <div className="item-b">item 2</div>
+    <div className="item-c">item 3</div>
+    <div className="item-d">item 4</div>
+    <div className="item-e"> item 5</div>
+    <div className="item-f"> item 6</div>
+    </div>
+    </>
+  )
+}
